ac506df1bb63c85c8010a44c4def351d7250583c
Tyler Hevia <tylerhevia@gmail.com>
2017-05-13 12:20:05 -0600

    input fields and js variables

---
+++ script.js
@@ -0,0 +1,3 @@
+var websiteTitleInput = document.querySelector('.website-title-input');
+var websiteURLInput = document.querySelector('.website-URL-input');
+var enterButton = document.querySelector('.enter-button');
