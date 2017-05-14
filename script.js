28547e97c10498e80174947dd42d7bc744f97f8b
Molly Foster <mollyrfoster@gmail.com>
2017-05-14 17:52:43 -0600

    update HTML, CSS, toggle class .read with jquery

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 var websiteTitleInput = document.querySelector('.website-title-input');
 var websiteURLInput = document.querySelector('.website-URL-input');
 var enterButton = document.querySelector('.enter-button');
+
+//mark button as read
+$('.read-button').on('click', function() {
+  $(this).toggleClass('read');
+});
