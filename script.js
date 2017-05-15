11910d92403ded25d215d80f1be527c9006fc652
Tyler Hevia <tylerhevia@gmail.com>
2017-05-15 10:03:45 -0600

    whatever

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 var websiteTitleInput = document.querySelector('.website-title-input');
-var websiteURLInput = document.querySelector('.website-URL-input');
+var websiteURLInput = document.querySelector('.website-url-input');
 var enterButton = document.querySelector('.enter-button');
+
+var websiteName
+var urlName
+
+
+enterButton.addEventListener('click', function()  {
+  event.preventDefault();
+  console.log ('yo');
+   websiteName = websiteTitleInput.value;
+   urlName = websiteURLInput.value;
+});
+
+$('.enter-button').on('click', function () {     // Appending text from input fields to right side
+  websiteName = $('.website-title-input').val();
+  urlName = $('.website-url-input').val();
+  $('.li-website-title').text(websiteName);
+  $('.li-website-url').text(urlName);
+  $('.right-side').append($ ('.website-title-input').val() );
+  $('.right-side').append($ ('.website-url-input').val() );
+});
