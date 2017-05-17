17ddedb2dfa313a095b3ba74ef80c91d951512de
Molly Foster <mollyrfoster@gmail.com>
2017-05-16 20:30:29 -0600

    small changes to HTML,CSS. Button/bookmark functionality in JS

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-var websiteTitleInput = document.querySelector('.website-title-input');
-var websiteURLInput = document.querySelector('.website-URL-input');
-var enterButton = document.querySelector('.enter-button');
+var websiteName = $('.website-title-input').val();
+var websiteURL = $('.website-url-input').val();
+var enterButton = $('.enter-button');
+var bookmarks = [];
+
+$('.enter-button').on('click', function() {          // create new bookmark with inputted title and url
+  event.preventDefault();
+  websiteName = $('.website-title-input').val();
+  websiteURL = $('.website-url-input').val();
+  console.log("hello");
+  $('.right-side').append(`<div class="website-info">
+    <p class="website-name"> ${websiteName} </p><hr>
+    <a class="website-url" href="${websiteURL}" target="_blank">${websiteURL}</a><hr>
+    <button class="read-button" type="submit" value="Read">Read</button>
+    <button class="remove-button" type="submit" value="Remove">Remove</button>
+  </div>`);
+});
+
+//push each new card to an array
+$('.enter-button').on('click', function() {
+    bookmarks.push($('.website-title-input').val());
+      console.log(bookmarks);
+});
 
 
-//mark button as read
-$('.read-button').on('click', function() {
+//write a counting function- count length of array and show #, run function every time the enter button is clicked
+//write a function on removal of card: get title, find title in array, remove from array completely
+//run count function again to reestablish length of array
+//display number on interface
+
+$('.enter-button').on('click', function() {      // clearing input fields on click
+  event.preventDefault();
+  websiteName = $('.website-title-input').val('');
+  websiteURL = $('.website-url-input').val('');
+});
+
+// mark button as read
+$('.right-side').on('click', 'button.read-button', function() {
   $(this).toggleClass('read');
+  $(this).parent().toggleClass('backgroundColor');
+});
+
+//remove bookmark entirely
+$('.right-side').on('click', '.remove-button', function() {
+    $(this).parent('.website-info').remove();
 });
