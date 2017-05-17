ddf3ccd0a822af6640060e6ba9c770fbda7eb029
Tyler Hevia <tylerhevia@gmail.com>
2017-05-16 20:42:02 -0600

    js refactoring, responsiveness, disable button

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
-var websiteTitleInput = document.querySelector('.website-title-input');
-var websiteURLInput = document.querySelector('.website-url-input');
+var websiteTitleInput = $('.website-title-input');
+var websiteURLInput = $('.website-url-input');
 var enterButton = document.querySelector('.enter-button');
+var alertMessage = 'Please enter a website name and a URL';
 
-var websiteName
-var urlName
+$(".website-title-input").keyup(function() {
+  changeDisabledState();
+})
+// Can I combine these two?
+$(".website-url-input").keyup(function() {
+  changeDisabledState();
+})
 
-
-enterButton.addEventListener('click', function()  {
+// create new bookmark with inputted title and url
+enterButton.addEventListener('click', function() {
   event.preventDefault();
-  console.log ('yo');
-   websiteName = websiteTitleInput.value;
-   urlName = websiteURLInput.value;
+  changeDisabledState();
+  var websiteName = websiteTitleInput.val();
+  var websiteURL = websiteURLInput.val();
+  createBookmark(websiteName, websiteURL);
+  // clearing input fields on click
+})
+
+function changeDisabledState() {
+  if (websiteTitleInput.val() !== "" && websiteURLInput.val() !== "") { return enterButton.disabled = false }
+  return enterButton.disabled = true;
+}
+
+function clearInputFields() {
+  websiteTitleInput.val("");
+  websiteURLInput.val("");
+}
+
+function createBookmark(name, url) {
+  $('.right-side').append(`<div class="website-info">
+    <p class="website-name"> ${name} </p><hr>
+    <p class="website-url"> ${url} </p><hr>
+    <button class="read-button" type="submit" value="Read">Read</button>
+    <button class="remove-button" type="submit" value="remove">remove</button>
+  </div>`);
+  clearInputFields();
+}
+
+
+//remove bookmark entirely
+$('.right-side').on('click', '.remove-button', function() {
+   $(this).parents('.website-info').remove();
 });
 
-$('.enter-button').on('click', function () {     // Appending text from input fields to right side
-  websiteName = $('.website-title-input').val();
-  urlName = $('.website-url-input').val();
-  $('.li-website-title').text(websiteName);
-  $('.li-website-url').text(urlName);
-  $('.right-side').append($ ('.website-title-input').val() );
-  $('.right-side').append($ ('.website-url-input').val() );
-
-//mark button as read
-$('.read-button').on('click', function() {
-  $(this).toggleClass('read');
-});
+// // mark button as read
+// $('.right-side').on('click', function() {
+//   $(this).toggleClass('read-button');
+// });
