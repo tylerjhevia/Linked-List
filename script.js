ff517316adb8f04c7690ac08b22683e6e5c6a8fe
Tyler Hevia <tylerhevia@gmail.com>
2017-05-17 16:01:16 -0600

    counters and stuff

--- script.js
+++ script.js
@@ -5,9 +5,39 @@ var enterButton = document.querySelector('.enter-button');
   // function validateURL() {if(/^(http|https|ftp):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i.test($("#url").val())){
   //     alert("valid URL");
   // } else {
-  //     return alert("invalid URL");
+  //     return console.log("invalid URL");
   // }}
 
+
+  var bookmarkCount = 0;
+  var readBookmarkCount = 0;
+  var unreadBookmarkCount= 0;
+
+  function countReadBookmarks() {
+    readBookmarkCount += 1
+    var readBookmarks = document.querySelector(".read-bookmarks");
+    return readBookmarks.innerHTML = readBookmarkCount;
+    }
+
+function countBookmarks() {
+  bookmarkCount += 1;
+  console.log(bookmarkCount);
+  var bookmarks = document.querySelector(".bookmarks");
+  bookmarks.innerHTML = bookmarkCount
+}
+
+function countUnreadBookmarks() {
+  unreadBookmarkCount = bookmarkCount - readBookmarkCount;
+  var unreadBookmarks = document.querySelector(".unread-bookmarks");
+  unreadBookmarks.innerHTML = unreadBookmarkCount;
+}
+
+function removeBookmarks() {
+  bookmarkCount -= 1;
+  console.log(bookmarkCount);
+  var bookmarks = document.querySelector(".bookmarks");
+  bookmarks.innerHTML = bookmarkCount
+}
 
 $(".website-title-input").keyup(function() {
   changeDisabledState();
@@ -20,12 +50,14 @@ $(".website-url-input").keyup(function() {
 // create new bookmark with inputted title and url
 enterButton.addEventListener('click', function() {
   event.preventDefault();
-  changeDisabledState();
-  // validateURL();
   var websiteName = websiteTitleInput.val();
   var websiteURL = websiteURLInput.val();
   createBookmark(websiteName, websiteURL);
+  // counting bookmarks
+  countBookmarks();
+  countUnreadBookmarks();
   // clearing input fields on click
+  changeDisabledState();
 })
 
 function changeDisabledState() {
@@ -43,7 +75,7 @@ function createBookmark(name, url) {
     <p class="website-name"> ${name} </p><hr>
     <a href="${url}" target="_blank"  class="website-url"> ${url} </a><hr>
     <button class="read-button" type="submit" value="Read">Read</button>
-    <button class="remove-button" type="submit" value="remove">remove</button>
+    <button class="remove-button" type="submit" value="remove">Remove</button>
   </div>`);
   clearInputFields();
 }
@@ -53,9 +85,19 @@ function createBookmark(name, url) {
 $('.right-side').on('click', 'button.read-button', function() {
  $(this).toggleClass('read');
  $(this).parent().toggleClass('backgroundColor');
+ countReadBookmarks();
+ countUnreadBookmarks();
 });
 
 //remove bookmark entirely
 $('.right-side').on('click', '.remove-button', function() {
    $(this).parent('.website-info').remove();
+   removeBookmarks();
+   countUnreadBookmarks();
+});
+
+//remove all read bookmarks
+$('.clear-all-button').on('click', function(event){
+ event.preventDefault();
+ $('.website-info').remove('.backgroundColor');
 });
